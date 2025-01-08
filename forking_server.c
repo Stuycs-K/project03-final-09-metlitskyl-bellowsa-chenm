4856fcb7ab71c882cee834b0dab89f9284a44187
abellows50 <abellows50@stuy.edu>
2025-01-07 22:19:40 -0500

    coppied in lab15 forking server logic

--- forking_server.c
+++ forking_server.c
@@ -1,64 +1,69 @@
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
+#include "pipe_networking.h"
+#include <fcntl.h>
 #include <unistd.h>
-#include "utils.h"
-#define PORT 8080
+#include <signal.h>
+#include <sys/wait.h>
 
 
-int main(int argc, char const* argv[])
-{
-    int server_fd, new_socket;
-    ssize_t valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    socklen_t addrlen = sizeof(address);
+void sighandler(int signo){
+  switch(signo){ //child 
+    case SIGINT:
+      {
+        remove("WKP");
+        perror("Exiting due to ctrl-c...");
+        exit(0);
+      }
+    case SIGCHLD:
+      {
+        printf("reaping child...\n");
+        while(waitpid(-1,NULL,0));
+      }
+    // cleanup();
+  }
+
+}
+
+void rando(int * r){
+  int fd = open("/dev/random", O_RDONLY, 0);
+  read(fd, r, sizeof(int));
+  *r = *r % 50 + 50;
+  close(fd);
+}
+
+
+void server_cycle(int to_client, int from_client){
+  //actual thing it does
+  int r;
+  rando(&r);
+  int bytes;
+  while(1){
+    bytes = write(to_client, &r, sizeof(r));
+
+    if(bytes != 0 && bytes != -1){
+      printf("(%d) sent %d\n",getpid(),r);
+      rando(&r);
+      sleep(1);
+    }
+    else{
+      break;
+    }
     
-    // Creating socket file descriptor
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    v_err(server_fd, "socket creation err: ", 1);
+  }
+  printf("client broke connection...\n\n");
+}
 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET,
-                   SO_REUSEADDR | SO_REUSEPORT, &opt,
-                   sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+int main() {
+  signal(SIGINT, sighandler);
+  signal(SIGCHLD, sighandler);
+  while(1){ 
+    //main server loop
+    printf("establishing connection to client...\n");
+    int from_client = server_setup(); //wait for connection...
+    printf("recived client...\n");
+    printf("forking...\n");
+    if(fork()==0){//if fork is child
+      int to_client = server_connect(from_client);
+      server_cycle(to_client, from_client);
     }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket
-         = accept(server_fd, (struct sockaddr*)&address,
-                  &addrlen))
-        < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read(new_socket, buffer,
-                   1024 - 1); // subtract 1 for the null
-                              // terminator at the end
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-
-    // closing the connected socket
-    close(new_socket);
-    // closing the listening socket
-    close(server_fd);
-    return 0;
+  }
 }
