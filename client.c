d43965c95e756cb6d35d433e23a846171b98cdd3
abellows50 <abellows50@stuy.edu>
2025-01-07 23:38:13 -0500

    went throuh to undertsand all socket fxns from GEEKS axample code. Then simplified and added error handling

--- client.c
+++ client.c
@@ -5,45 +5,43 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#include "utils.h"
+
 #define PORT 8080
 
-int main(int argc, char const* argv[])
-{
-    int status, valread, client_fd;
-    struct sockaddr_in serv_addr;
-    char* hello = "Hello from client";
-    char buffer[1024] = { 0 };
-    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
+int main(int argc, char const* argv[]){
+    
 
+    //create socket
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    v_err(client_fd, "socket creation err", EXIT);
+
+    //create sockaddr_in for serv_addr
+    struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary
-    // form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
-        <= 0) {
-        printf(
-            "\nInvalid address/ Address not supported \n");
-        return -1;
-    }
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    int conversion = inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    v_err(conversion, "conversion of ip addr err", EXIT);
 
-    if ((status
-         = connect(client_fd, (struct sockaddr*)&serv_addr,
-                   sizeof(serv_addr)))
-        < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
+    //attatch client_fd to server
+    int status = connect(client_fd, (struct sockaddr*)&serv_addr,sizeof(serv_addr)); 
+    v_err(status, "connection err", EXIT);
+        
     while(1){
-    send(client_fd, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    valread = read(client_fd, buffer,
-                   1024 - 1); // subtract 1 for the null
-                              // terminator at the end
-    printf("%s\n", buffer);
+        //what the client actually does
+        char* hello = "Hello from client";
+        char buffer[1024] = { 0 };
+
+        write(client_fd, hello, strlen(hello));
+        printf("Hello message sent\n");
+        int valread;
+        valread = read(client_fd, buffer,
+                    1024 - 1); // subtract 1 for the null
+                                // terminator at the end
+        printf("%s\n", buffer);
     }
     // closing the connected socket
     close(client_fd);
