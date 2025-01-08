ce06da31f67f663ded1b3c8f468ed001539fd3e8
leomet07 <leomet07@gmail.com>
2025-01-08 14:42:38 -0500

    design patch struct with matthew

--- utils.c
+++ utils.c
@@ -1,13 +1,29 @@
 #include "utils.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int v_err(int sigerr, char * msg, int _exit){
-    if(sigerr < 0){
+#define MAX_FILEPATH 512
+
+#define MODE_TOUCH 0
+#define MODE_MODIFY 1 // modifying TEXT inside a file
+#define MODE_REMOVE 2
+
+#define MODE_PLUS 3
+#define MODE_MINUS 4
+
+int v_err(int sigerr, char *msg, int _exit) {
+    if (sigerr < 0) {
         perror(msg);
-        if (_exit){
+        if (_exit) {
             exit(1);
         }
     }
     return sigerr;
 }
+
+struct patch {
+    char filepath[MAX_FILEPATH];
+    int MODE;
+    size_t memory_size;
+    void *memory; // raw memory, used for chars in mode_touch, used for our diff format in mode_modify, 1 byte for mode_remove
+};
