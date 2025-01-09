b9ecf85980c603ff3ac1201ec13bb42c17357024
leomet07 <leomet07@gmail.com>
2025-01-08 20:04:25 -0500

    fix utils

--- utils.c
+++ utils.c
@@ -2,15 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_FILEPATH 512
-
-#define MODE_TOUCH 0
-#define MODE_MODIFY 1 // modifying TEXT inside a file
-#define MODE_REMOVE 2
-
-#define MODE_PLUS 3
-#define MODE_MINUS 4
-
 int v_err(int sigerr, char *msg, int _exit) {
     if (sigerr < 0) {
         perror(msg);
@@ -21,9 +12,7 @@ int v_err(int sigerr, char *msg, int _exit) {
     return sigerr;
 }
 
-struct patch {
-    char filepath[MAX_FILEPATH];
-    int MODE;
-    size_t memory_size;
-    void *memory; // raw memory, used for chars in mode_touch, used for our diff format in mode_modify, 1 byte for mode_remove
-};
+void err() {
+    printf("ERROR %d: %s\n", errno, strerror(errno));
+    exit(errno);
+}
--- utils.h
+++ utils.h
@@ -1,6 +1,30 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #ifndef utils_h
 #define utils_h
 
-int v_err(int sigerr, char * msg, int _exit);
+int v_err(int sigerr, char *msg, int _exit);
+void err();
 
+#define MAX_FILEPATH 512
+
+#define MODE_TOUCH 0
+#define MODE_MODIFY 1 // modifying TEXT inside a file
+#define MODE_REMOVE 2
+
+#define MODE_PLUS 3
+#define MODE_MINUS 4
+
+struct patch {
+    char filepath[MAX_FILEPATH];
+    int mode;
+    size_t memory_size;
+    char memory[]; // FLEXIBLE ARRAY MEMBBER, unkown length of memory raw memory, used for chars in mode_touch, used for our diff format in mode_modify, 1 byte for mode_remove
+};
 #endif
