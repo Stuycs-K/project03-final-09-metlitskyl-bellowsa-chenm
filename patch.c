46c83ceb2b0de73970077a12b568dd6830cf11c7
leomet07 <leomet07@gmail.com>
2025-01-08 20:05:19 -0500

    update makefile for patch file; create a new patch and write it!

---
+++ patch.c
@@ -0,0 +1,46 @@
+#include "utils.h"
+
+struct patch *create_patch(char *filepath, int mode, size_t memory_size, char *memory) {
+    struct patch *patch = calloc(1, sizeof(struct patch) + memory_size * sizeof(char));
+
+    strcpy(patch->filepath, filepath);
+    patch->mode = mode;
+    patch->memory_size = memory_size;
+    memcpy(patch->memory, memory, memory_size);
+
+    return patch;
+}
+
+void visualize_patch(struct patch *patch) {
+    printf("----Visualizing Patch: ------\n");
+    printf("filepath: |%s|\n", patch->filepath);
+    printf("memory size: |%d|\n", patch->memory_size);
+    printf("raw memory: |%s|\n", patch->memory);
+    printf("----End visualizing. ------\n");
+}
+
+void write_patch(char *filename, struct patch *patch) {
+    int out_file = open(filename, O_CREAT | O_WRONLY, 0644);
+    if (out_file == -1) {
+        err();
+    }
+    size_t struct_byte_size = sizeof(struct patch) + patch->memory_size; // min size is sizeof(struct patch)
+
+    int write_status = write(out_file, patch, struct_byte_size);
+    if (write_status == -1) {
+        err();
+    }
+}
+
+int main() {
+    char txt[] = "hi\nline2\nline3";
+    struct patch *mypatch = create_patch("hi.txt", MODE_TOUCH, strlen(txt) + 1, txt);
+    visualize_patch(mypatch);
+
+    char filename[] = ".dit/patch1.patch";
+    printf("Writing patch...\n");
+    write_patch(filename, mypatch);
+    printf("Patch written!\n");
+
+    return 0;
+}
