723762c049d7a93791b752097dac9b8a3ee2b74a
mc3297 <hyperion3297@gmail.com>
2025-01-10 14:44:13 -0500

    added important debug comments

--- diff.c
+++ diff.c
@@ -16,6 +16,7 @@ void diff(const char *a, const char *b) {
 
     for (int d = 0; d <= MAX; d++) {
         trace[d] = malloc((2*MAX+1)*sizeof(Point));
+	memset(trace[d], 3, (2*MAX+1)*sizeof(Point));
 
         for (int k = -d; k <= d; k += 2) {
             int x, y;
@@ -29,6 +30,7 @@ void diff(const char *a, const char *b) {
 
             y = x-k;
             trace[d][k+MAX] = (Point){x, y};
+            printf("%d: trace: %d %d\n", d, x, y);
 
             while (x < N && y < M && a[x] == b[y]) {
                 x++;
@@ -43,6 +45,12 @@ void diff(const char *a, const char *b) {
                     Point p = trace[d_back][k+MAX];
                     printf("(%d, %d)\n", p.x, p.y);
                 }*/
+                for (int i = 0; i <= d; i++) {
+		    for (int j = 0; j <= 2*MAX; j++) {
+			printf("(%d,%d) ", trace[i][j].x, trace[i][j].y);
+		    }
+		    printf("\n");
+		}
                 printf("min edit dist is %d\n", d);
                 free(v);
                 return;
@@ -52,8 +60,9 @@ void diff(const char *a, const char *b) {
 }
 
 int main() {
-    const char *a = "ABCABBA";
-    const char *b = "CBABAC";
+	//gcc -Wall diff.c && ./a.out > out.txt && sed 's/50529027/_/g' out.txt
+    const char *a = "matthew";
+    const char *b = "little";
 
     diff(a, b);
     return 0;
