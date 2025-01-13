c23b1ff6964e940e54f9fa14c06f6611ffab5cec
leomet07 <leomet07@gmail.com>
2025-01-13 14:22:35 -0500

    get latest commit number from .dit folder

--- add.c
+++ add.c
@@ -1,6 +1,62 @@
+#include "utils.h"
+
 int main(int argc, char *argv[]) {
-    // 1. get filename
-    // 2. go through git tree
+    char tracked_dir[] = "dit_test_dir/";
+
+    char dit_folder[MAX_FILEPATH];
+    strcat(dit_folder, tracked_dir);
+    strcat(dit_folder, ".dit/");
+
+    char filename[] = "hi.txt";
+    char filepath[MAX_FILEPATH];
+    strcat(filepath, tracked_dir);
+    strcat(filepath, filename);
+
+    char commit_folder[MAX_FILEPATH];
+    strcat(commit_folder, dit_folder);
+    strcat(commit_folder, "commits/");
+
+    printf("Filename : |%s|\n", filepath);
+    printf("Corresponding dit folder: |%s|\n", dit_folder);
+    printf("Corresponding dit commit folder: |%s|\n", commit_folder);
+
+    // 2. go through git tree commits folder
+
+    // get latest (highest #) commit
+    DIR *d;
+    d = opendir(commit_folder);
+    if (d == 0) {
+        err();
+    }
+
+    int max_commit_number = -1;
+
+    struct dirent *entry = NULL;
+    printf("Directories: \n");
+    while ((entry = readdir(d))) {
+        if (entry->d_type != DT_DIR) {
+            continue;
+        }
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+        // char *full_path = join_path(folder_path_with_slash, entry->d_name);
+        // total += my_ls(full_path);
+        int commit_number = -1;
+        sscanf(entry->d_name, "%d", &commit_number);
+
+        printf("commit number (%d) found\n", commit_number);
+
+        if (commit_number > max_commit_number) {
+            max_commit_number = commit_number;
+        }
+    }
+
+    if (max_commit_number == -1) {
+        printf("NO COMMITS MADE YET!\n");
+    } else {
+        printf("Latest commit is: %d\n", max_commit_number);
+    }
 
     // 3. see if file exists
     // 4. if so, send to diff code and make mod patch (store in staging dir)
--- utils.h
+++ utils.h
@@ -1,20 +1,22 @@
+#include "file_transfer.h"
+#include "user.h"
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+
 #include <sys/stat.h>
 #include <unistd.h>
-
-#include "file_transfer.h"
-#include "user.h"
 
 #ifndef utils_h
 #define utils_h
 
 #define EXIT 1
 #define NO_EXIT 0
-int v_err(int sigerr, char * msg, int _exit);
+int v_err(int sigerr, char *msg, int _exit);
 void err();
 
 #define MAX_FILEPATH 512
@@ -33,6 +35,15 @@ struct patch {
     char memory[]; // FLEXIBLE ARRAY MEMBBER, unkown length of memory raw memory, used for chars in mode_touch, used for our diff format in mode_modify, 1 byte for mode_remove
 };
 
-int get_repo_path(char * server_root, struct ft_init * init, char * target);
+int get_repo_path(char *server_root, struct ft_init *init, char *target);
+
+// yes i know this is defined in dirent.h it is just my intellisense is stupid
+#ifndef DT_REG
+#define DT_REG 8
+#endif
+
+#ifndef DT_DIR
+#define DT_DIR 4
+#endif
 
 #endif
