09bce6465ac69ec2061f0abcbe375f1d5e7cd2d4
leomet07 <leomet07@gmail.com>
2025-01-13 10:43:54 -0500

    scaffold add

---
+++ add.c
@@ -0,0 +1,8 @@
+int main(int argc, char *argv[]) {
+    // 1. get filename
+    // 2. go through git tree
+
+    // 3. see if file exists
+    // 4. if so, send to diff code and make mod patch (store in staging dir)
+    // 5. if not, make a simple touch commit
+}
