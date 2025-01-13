47c20c08445d99004cc0a331ed8c83b343172116
Abel Bellows <abellows50@stuy.edu>
2025-01-13 17:01:37 -0500

    now .dit transfer works

--- client.c
+++ client.c
@@ -52,10 +52,10 @@ int main(int argc, char const* argv[]){
 
     if (!strcmp(argv[1], "download")){
         //init connection and ask for a transmission
-        new_ft_init(TR_TRSMT, repo_name, &user, &init);
+        new_ft_init(TR_TRSMT, repo_dit_base_name, &user, &init);
         write(client_fd, &init, sizeof(struct ft_init));
         
-        recv_full_directory_contents(client_fd, "./");
+        recv_full_directory_contents(client_fd, "../");
         return 0;
     }
     else if(!strcmp(argv[1], "push")){
