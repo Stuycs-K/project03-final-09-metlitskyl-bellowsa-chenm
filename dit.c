e56503e8f9fa1c413924050cb0ec495b688562b4
Abel Bellows <abellows50@stuy.edu>
2025-01-17 14:45:22 -0500

    implemented dit push

--- dit.c
+++ dit.c
@@ -17,7 +17,9 @@ void print_usage(){
            "These are common dit commands used in various situations:\n\n"
 
            "create a repo to push to: \n"
-           "    init\n\n"
+           "    init\n"
+           "    download\n"
+           "    pull\n\n"
 
            "remote (pull and push files from server): \n"
            "    push\n\n"
@@ -49,15 +51,29 @@ int main(int argc, char * argv[]){
         exit(0);
     }
 
-    if(!strcmp(argv[1], "build")){
+    if(!strcmp(cmd,"pull")){
+        // printf("about to make argv2\n");
+        char * argv2[2];
+        argv2[0] =  argv[0];
+        argv2[1] = calloc(strlen("download")+1,sizeof(char));
+        strcpy(argv2[1], "download");
+        // printf("client\n");
+        client(2, argv2);
+        // printf("build\n");
+        build("./");
+        free(argv2[1]);
+        exit(0);
+    }
+
+    if(!strcmp(cmd, "build")){
         build("./");
         exit(0);
     }
-    if(!strcmp(argv[1], "add")){
+    if(!strcmp(cmd, "add")){
         add("./", argv[2]);
         exit(0);
     }
-    if(!strcmp(argv[1], "commit")){
+    if(!strcmp(cmd, "commit")){
         commit("./");
         exit(0);
     }
