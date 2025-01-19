b00a5ea72f51b98c4fb38cd5ec4da862a3b9ad60
abellows50 <abellows50@stuy.edu>
2025-01-19 11:43:29 -0500

    maybe done with download? testing

--- download.c
+++ download.c
@@ -27,52 +27,32 @@
 
 int download(int argc, char * argv[]){
 
-    char path_to_programdir[1024];
-
-    realpath(argv[0], path_to_programdir);
-    path_to_programdir[strlen(path_to_programdir) - strlen(__FILE__)  + 1] = 0;
-
-    int client_fd = setup_client();
-    printf("connected...\n");
-
-    struct ft_init init;
-    struct ft_user user;
-
-    init_client_config((char *)argv[0], &user);
-
-    char repo_name[512];
-    char repo_name_dit[512];
-    char repo_target[1050];
-
-    get_repo_name_from_cwd(repo_name, sizeof(repo_name), repo_name_dit, repo_target);
-
-    // if(made_new_user){
-    //     struct ft_init init_usr;
-    //     new_ft_init(TR_AINIT, "", &user, &init_usr);
-    //     write(client_fd, &init_usr, sizeof(struct ft_init));
-    // }
+    struct client_session * cs = malloc(sizeof(struct client_session));
+    new_client_session(argv, cs);
 
    
     //init connection and ask for a transmission
 
     //this stuff is for sound
     int kidid;
-    if (user.sound){
-        kidid = play_song(path_to_programdir,"kesh.mp3", 3);
+    if (cs->user.sound){
+        kidid = play_song(cs->path_to_programdir,"kesh.mp3", 3);
     }
 
-    new_ft_init(TR_TRSMT, repo_name, &user, &init);
-    write(client_fd, &init, sizeof(struct ft_init));
+    struct ft_init init;
+    new_ft_init(TR_TRSMT, cs->repo_name, &cs->user, &init);
+    write(cs->client_fd, &init, sizeof(struct ft_init));
     
     mkdir(".dit",0744); //just insure that there is a .dit
-    recv_full_directory_contents(client_fd, ".");
+    recv_full_directory_contents(cs->client_fd, ".");
 
     //kill the mpg123 if sound is on
-    if(user.sound){
+    if(cs->user.sound){
         kill(kidid, SIGKILL);
     }
 
-    close(client_fd);
+    close(cs->client_fd);
+    free(cs);
     printf("done!\n");
     return 0;
 }
