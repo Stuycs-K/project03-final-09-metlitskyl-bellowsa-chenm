737002430cb6d3b79e7cb5170944d24741d5d99d
leomet07 <leomet07@gmail.com>
2025-01-19 18:12:33 -0500

    continue to setup dit status

---
+++ status.c
@@ -0,0 +1,8 @@
+#include "diff.h"
+#include "patch.h"
+#include "utils.h"
+
+
+void status(char *tracked_dir){
+    printf("Dit status...\n");
+}
---
+++ status.h
@@ -0,0 +1,7 @@
+#ifndef STATUS_H
+
+#define STATUS_H
+
+void status(char *commit_folder);
+
+#endif
